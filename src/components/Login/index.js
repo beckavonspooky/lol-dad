841c5911bb8a8429b40471690dd7f549689a28ae
Rebeca Levy <beckalevy@Rebecas-MacBook-Pro.local>
2019-12-11 20:24:40 -0800

    login/register form edits

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
 import {withRouter} from 'react-router-dom'
 import { doSignInWithEmailAndPassword } from '../../firebase/firebase'
+import { Button, Form } from 'semantic-ui-react'
 
 
 import * as ROUTES from '../../constants/routes'
+
+// import login from '../../images/login.jpg'
+
+// const style={
+//     "width": "65em", 
+//     "height": "35em", 
+//     "backgroundPosition": "center",
+//     "backgroundRepeat": "no-repeat",
+//     "backgroundSize": "cover",
+//     "border": "none",
+//     "backgroundImage": `url(${login})`
+// }
 class Login extends Component {
     state = {
         email: '',
@@ -32,13 +45,13 @@ class Login extends Component {
         console.log(email, '<---- state change')
         return(
             <div>
-            <h4>Login</h4>
-            
+               
             <div className='Form'>
+            <h1>Login</h1>
                 <form onSubmit={this.onSubmit}>
-                    <input type='text' name='email'  placeholder="EMAIL" value={email} onChange={this.onChange}/>
-                    <input type='text' name='password' placeholder="PASSWORD" value={password} onChange={this.onChange}/>
-                    <input type='submit' value='submit' />
+                    <input type='text' name='email'  placeholder="EMAIL" value={email} onChange={this.onChange}/><br/>
+                    <input type='text' name='password' placeholder="PASSWORD" value={password} onChange={this.onChange}/><br/>
+                    <button type='submit'>Login</button>
                 
                 </form>
             </div>
